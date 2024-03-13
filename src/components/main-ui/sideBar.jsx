e595a4fa8702292326d36b37306d3a12ef3d515a
Ali Bin Ejaz <127725748+alibinejazz@users.noreply.github.com>
2024-03-14 04:13:39 +0500

    Update sideBar.jsx

--- src/components/main-ui/sideBar.jsx
+++ src/components/main-ui/sideBar.jsx
@@ -105,7 +105,7 @@ function SideBar({
       className="bg-[#fff] pb-[30px] px-[20px] rounded-3xl overflow-y-hidden"
       style={{ height: "420px" }}
     >
-      <h1 className="text-center p-2.5 text-[20px] font-semQuestionModalibold h-[69px] flex items-center justify-center backdrop-blur-sm border-b-[1px] border-b-[#F0F0F0] mb-2">
+      <h1 className="text-center p-2.5 text-[20px] font-semibold h-[69px] flex items-center justify-center backdrop-blur-sm border-b-[1px] border-b-[#F0F0F0] mb-2">
         {" "}
         Frequently Asked journeys{" "}
       </h1>
@@ -155,7 +155,7 @@ function SideBar({
       ) : (
         <div></div>
       )}
-<div className="max-h-[335px] pr-3 overflow-y-auto  sideBarQuestion">
+<div className="max-h-[335px] pr-3 overflow-y-auto overflow-x-hidden  sideBarQuestion">
       {questions.map((question, index) => (
         <div
           className={`bg-sidbar-color p-2.5 flex items-center mb-3 mt-3 gap-4 rounded-3xl w-[480px] ${
